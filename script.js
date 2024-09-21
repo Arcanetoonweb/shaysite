4690b98d3e22cca6ca5ffba1e685034559c6623b
Arcanetoonweb <jvvhgguhghggg@gmail.com>
2024-09-21 15:36:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,22 @@ function addToCart(productId) {
   // קריאה לפונקציה לצורך הצגת המוצרים בעגלת הקניות בעת טעינת הדף
   document.addEventListener('DOMContentLoaded', displayCart);
   
+  grantButton.addEventListener("click", function() {
+    localStorage.setItem("consentGranted", "true");
+    function gtag() { dataLayer.push(arguments); }
+
+    gtag('consent', 'update', {
+      ad_user_data: 'granted',
+      ad_personalization: 'granted',
+      ad_storage: 'granted',
+      analytics_storage: 'granted'
+    });
+  });
+
+  // Load gtag.js script.
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=GTM-N2Q967ZV';
+
+  var firstScript = document.getElementsByTagName('script')[0];
+  firstScript.parentNode.insertBefore(gtagScript,firstScript);
